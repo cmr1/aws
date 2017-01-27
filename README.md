fb52357d4643759bbf5a2260a430a6a67b61a0d6
Charlie McClung <charlie@cmr1.com>
2017-01-26 18:06:43 -0700

    Finished updating repo name references...

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/cmr1/node-aws.svg?branch=master)](https://travis-ci.org/cmr1/node-aws)
+[![Build Status](https://travis-ci.org/cmr1/aws-node.svg?branch=master)](https://travis-ci.org/cmr1/aws-node)
 
 # aws-node (cmr1-aws)
 NodeJS AWS service helper classes
--- hooks/post-version.js
+++ hooks/post-version.js
@@ -8,7 +8,7 @@ const packageInfo = require(path.join(rootDir, 'package.json'));
 console.log(`Updating markdown files with tagged TravisCI badge to version: "${packageInfo.version}"`);
 
 replace({
-	regex: /(https\:\/\/travis\-ci\.org\/cmr1\/node-aws\.svg\?branch\=|\|\s*)v[0-9]+\.[0-9]+\.[0-9]+(\s*\|)?/g,
+	regex: /(https\:\/\/travis\-ci\.org\/cmr1\/aws-node\.svg\?branch\=|\|\s*)v[0-9]+\.[0-9]+\.[0-9]+(\s*\|)?/g,
 	replacement: `$1v${packageInfo.version}$2`,
 	paths: [
 		rootDir
